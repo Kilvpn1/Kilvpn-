271b09bf1dc403c7489c6b7cfef28ea1b5748158
Kilvpn1 <dallapicolakildren@gmail.com>
2026-03-24 21:40:45 -0300

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,29 @@
+@rem Gradle startup script for Windows
+@rem (para quem buildar no PC também)
+@if "%DEBUG%" == "" @echo off
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+exit /b 1
+
+:execute
+"%JAVA_EXE%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:end
+if "%ERRORLEVEL%"=="0" goto mainEnd
+exit 1
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
